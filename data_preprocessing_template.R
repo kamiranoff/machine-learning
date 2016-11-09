8a4a6e83f69d62329003fc638d7887068696e94b
Kevin Amiranoff <kamiranoff@gmail.com>
2016-11-09 23:53:59 +0100

    replace missing values with mean

--- data_preprocessing_template.R
+++ data_preprocessing_template.R
@@ -2,3 +2,16 @@
 
 # Importing the dataset
 dataset = read.csv('Data.csv')
+
+
+
+# Replace missing values with the mean (the average)
+#is.na = is missing..
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                        dataset$Salary)
+      
--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -23,3 +23,12 @@ x = dataset.iloc[:, :-1].values
 # DEPENDANT Variables Vector
 # storing the last column.
 y = dataset.iloc[:, 3].values
+
+
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values= 'NaN', strategy='mean', axis=0)
+
+# fit to colum at index 1 and 2 age and salary -> Upperbound not included
+imputer = imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
