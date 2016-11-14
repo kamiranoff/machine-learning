a87a8a518088eddfe7aac589165d4e39c09a6718
Kevin Amiranoff <kamiranoff@gmail.com>
2016-11-14 01:16:06 +0100

    encodes variables

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -18,17 +18,37 @@ dataset = pd.read_csv('Data.csv')
 # iloc --> first argument is lines, second is columns.
 # : means we are taking all the lines
 # :-1 means we exclude the last one which is a dependant variable
-x = dataset.iloc[:, :-1].values
+X = dataset.iloc[:, :-1].values
 
 # DEPENDANT Variables Vector
 # storing the last column.
-y = dataset.iloc[:, 3].values
+Y = dataset.iloc[:, 3].values
 
 
 # Taking care of missing data
 from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values= 'NaN', strategy='mean', axis=0)
+imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
 
 # fit to colum at index 1 and 2 age and salary -> Upperbound not included
-imputer = imputer.fit(x[:, 1:3])
-x[:, 1:3] = imputer.transform(x[:, 1:3])
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+
+#Country
+# Encode country column into numbers
+labelEncoder_x = LabelEncoder()
+X[:, 0] = labelEncoder_x.fit_transform(X[:, 0])
+
+# Make a specific column for each entry in the column with boolean value
+# so there is order in categorical variables
+oneHotEncoder = OneHotEncoder(categorical_features=[0])
+X = oneHotEncoder.fit_transform(X).toarray()
+
+
+#Purchased
+labelEncoder_y = LabelEncoder()
+Y = labelEncoder_y.fit_transform(Y)
+
+print(X)
